f181834b559cad2434df9a7352303ebbc2cc6c06
fizzzgen <fizzzgen@gmail.com>
2020-03-31 01:23:15 +0300

    m

---
+++ job_price_update.py
@@ -0,0 +1,29 @@
+import config
+import sqlite3
+import requests
+import json
+import time
+import logging
+import datetime
+
+conn = sqlite3.connect(config.DB_PATH)
+cur = conn.cursor()
+
+
+for pair in config.PAIRS:
+    url = 'https://poloniex.com/public?command=returnOrderBook&currencyPair={}&depth=1'.format(pair)
+    data = json.loads(requests.get(url).text)
+    print(data)
+    buy = float(data['asks'][0][0])
+    sell = float(data['bids'][0][0])
+    avg = (buy + sell) / 2
+    ts = datetime.datetime.utcnow().timestamp()
+    query = 'INSERT INTO price(ts,avg,buy,sell,pair) VALUES({},{},{},{},"{}")'.format(
+        ts, avg, buy, sell, pair
+    )
+    logging.info(query)
+    cur.execute(query)
+    conn.commit()
+
+
+conn.close()
