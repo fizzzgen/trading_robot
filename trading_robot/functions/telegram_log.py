cadfaef31ac14a5086a1ef5c77c16dde310c5c23
fizzzgen <fizzzgen@gmail.com>
2020-04-02 23:39:52 +0000

    telegram not working in yandex cloud

--- trading_robot/functions/telegram_log.py
+++ trading_robot/functions/telegram_log.py
@@ -1,17 +1,7 @@
-import telebot
-
-from conf import config
-
-
-# u need to write to bot first to give him access
-_bot = telebot.TeleBot(config.TELEGRAM_TOKEN)
-_bot_important = telebot.TeleBot(config.IMPORTANT_TELEGRAM_TOKEN)
-_admin = config.TELEGRAM_ADMIN
-
 
 def online_log(text):
-    _bot.send_message(_admin, text, parse_mode='Markdown')
-
+    pass
 
 def online_log_important(text):
-    _bot_important.send_message(_admin, text, parse_mode='Markdown')
+    online_log("!!! \n\n\n" + text + "\n\n\n !!!")
+
