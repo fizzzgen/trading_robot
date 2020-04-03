a60ed6bd69925d537b411dc0271e5151145716ba
fizzzgen <fizzzgen@gmail.com>
2020-04-03 13:01:55 +0000

    Added email logs

--- trading_robot/functions/telegram_log.py
+++ trading_robot/functions/telegram_log.py
@@ -1,7 +1,37 @@
+import smtplib
+import datetime
+import time
+
+important_aggregator = []
+aggregator = []
+latest_important = 0.0
+latest = 0.0
+
+mail = 'fizzzgen@yandex.ru'
+password = '48674867'
+
+def send_emails(title,msg):
+    server = smtplib.SMTP_SSL('smtp.yandex.com:465')
+    server.login(mail,password)
+    server.auth_plain()
+    message = 'Subject: {}\n\n{}'.format(title,msg)
+    server.sendmail(mail ,mail,message)
+    server.quit()
+    print('E-mails successfully sent!')
 
 def online_log(text):
-    pass
+    global latest, aggregator
+    aggregator.append(text)
+    if latest < time.time() - 5 * 60 * 1000:
+        send_emails('TRADE INFO', '\n'.join(aggregator))
+        aggregator = []
+        latest = time.time()
+
 
 def online_log_important(text):
-    online_log("!!! \n\n\n" + text + "\n\n\n !!!")
-
+    global latest_important, important_aggregator
+    important_aggregator.append(text)
+    if latest_important < time.time() - 5 * 60:
+        send_emails('TRADE IMPORTANT', '\n'.join(important_aggregator))
+        important_aggregator = []
+        latest_important = time.time()
