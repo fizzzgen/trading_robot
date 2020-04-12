13970c594d8216f8f28c3b7f5677189e2085c9a6
fizzzgen <fizzzgen@gmail.com>
2020-04-12 22:15:15 +0000

    m

--- setup_and_run.sh
+++ setup_and_run.sh
@@ -5,4 +5,5 @@ systemctl daemon-reload
 systemctl start job_predict.service
 systemctl start job_price_update.service
 systemctl start job_orders_engine.service
+systemctl start job_flask.service
 systemctl status job_predict.service job_price_update.service job_orders_engine.service
