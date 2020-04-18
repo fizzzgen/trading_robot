408619b305644194da06330dc241d72771cb2663
fizzzgen <fizzzgen@gmail.com>
2020-04-19 01:48:38 +0300

    deleted garbage

---
+++ trading_robot/run_tests.sh
@@ -0,0 +1,3 @@
+python -m pytest test/test_buy_no_prediction.py
+python -m pytest test/test_buy_true_prediction.py
+python -m pytest test/test_sell.py
--- trading_robot/test/test_sell.py
+++ trading_robot/test/test_sell.py
@@ -5,7 +5,7 @@ from conf import config
 _pair = 'btc_usd'
 
 
-def test_process_buy_no_prediction():
+def test_sell():
 
     def patched_configure_poloniex1():
         polo = mock.Mock()
@@ -16,5 +16,5 @@ def test_process_buy_no_prediction():
     patch1 = mock.patch('conf.config.DB_PATH', '.{}'.format(random.randint(10000,90000)))
     patch2 = mock.patch('orders_engine_helpers.functions.configure_poloniex', patched_configure_poloniex1)
     with patch1, patch2:
-        from orders_engine_helpers import process_buy
-        process_buy.process_buy(_pair)
+        from orders_engine_helpers import process_sell
+        process_sell.process_sell(_pair)
