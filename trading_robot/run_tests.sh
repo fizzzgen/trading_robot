ecfc430f926d2f775f11f2e07813c2215fe9a749
fizzzgen <fizzzgen@gmail.com>
2020-04-22 15:20:01 +0300

    added pylint tests

--- trading_robot/run_tests.sh
+++ trading_robot/run_tests.sh
@@ -1,6 +1,15 @@
 apt-get install python3-pip
+pip3 install pylint
 pip3 install -r ../requirements.txt
 pip3 install pytest-xdist
 python3 -m pytest -n 10 test/
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 rm \.*
+mkdir ~/.pylint/
+echo "pylint..."
+pylint *.py > ~/.pylint/test.log
+pylint */*.py >> ~/.pylint/test.log
+pylint */*/*.py >> ~/.pylint/test.log
+echo 'pylint log: ~/.pylint/test.log'
+echo 'total errors file length:'
+wc -l ~/.pylint/test.log
