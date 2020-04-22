185d5182d00250951044a7f7e34d1d1eed138ee2
fizzzgen <fizzzgen@gmail.com>
2020-04-22 17:51:43 +0300

    fully tested move_orders

--- trading_robot/test/test_move.py
+++ trading_robot/test/test_move.py
@@ -100,3 +100,53 @@ def test_buy__order_move():
             assert len(transaction) == 1
             assert transaction[0].id == order_number2
             assert transaction[0].price == sell_price * config.ORDERBOOK_FORCER_MOVE_PERCENT
+
+
+def test_sell__order_move():
+    order_number = 1
+    order_number2 = 2
+
+    sell_price = 100
+    buy_price = 200
+    def patched_configure_poloniex():
+        polo = mock.Mock()
+        polo.returnCompleteBalances.return_value = {
+            config.get_pair_first_symbol(_pair): {'available': 100.0},
+            config.get_pair_second_symbol(_pair): {'available': 100.0},
+        }
+
+        polo.returnOpenOrders.return_value = [{'type': 'sell', 'orderNumber': order_number, 'rate': 1, 'amount': 1}]
+        polo.returnTradeHistory.return_value = []
+        polo.moveOrder.return_value = {'type': 'sell', 'orderNumber': order_number2, 'rate': 1, 'amount': 1}
+        return polo
+
+    patch1 = mock.patch('conf.config.DB_PATH', '.{}'.format(random.randint(10000,90000)))
+    patch2 = mock.patch('orders_engine_helpers.functions.configure_poloniex', patched_configure_poloniex)
+    with patch1, patch2:
+        from orders_engine_helpers import process_move_orders
+        from conf import database_setup as db
+        with db.session_scope() as session:
+            session.add(
+                db.Price(
+                    buy=buy_price,
+                    sell=sell_price,
+                    avg=None,
+                    pair=_pair,
+                )
+            )
+            session.add(
+                db.Transaction(
+                    id=order_number,
+                    ts=datetime.datetime.utcnow().timestamp() - config.DROP_BUY_ORDER_DELAY + 100,
+                    status=config.TransactionStatus.ON_STOP,
+                    type=config.TransactionType.SELL,
+                    amount=1,
+                    price=1,
+                )
+            )
+        process_move_orders.move_orders(_pair)
+        with db.session_scope() as session:
+            transaction = session.query(db.Transaction).all()
+            assert len(transaction) == 1
+            assert transaction[0].id == order_number2
+            assert transaction[0].price == buy_price / config.ORDERBOOK_FORCER_MOVE_PERCENT
